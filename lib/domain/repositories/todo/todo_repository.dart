b2a7aeda505dfe9b89d7edf7d081c27ab0bcba56
Zudah228 <rezel95.hin@gmail.com>
2023-11-08 22:35:58 +0900

    feat: TodoRepository に delayed を追加

--- lib/domain/repositories/todo/todo_repository.dart
+++ lib/domain/repositories/todo/todo_repository.dart
@@ -1,12 +1,16 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_riverpod_demo/domain/entities/todo/todo.dart';
 
-final todoRepositoryProvider = Provider.autoDispose((_) => const TodoRepository());
+final todoRepositoryProvider = Provider.autoDispose(
+  (_) => const TodoRepository(),
+);
 
 class TodoRepository {
   const TodoRepository();
 
   Future<List<Todo>> list() async {
+    await Future.delayed(const Duration(milliseconds: 500));
+
     return [
       Todo.uuid(title: 'Element 1'),
       Todo.uuid(title: 'Element 2'),
