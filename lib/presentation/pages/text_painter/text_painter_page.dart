7e7b5fec21189003ec2c5588d04dae737f3dbeab
Zudah228 <rezel95.hin@gmail.com>
2023-12-02 14:33:21 +0900

    TextPainter の実験追加

--- lib/presentation/pages/text_painter/text_painter_page.dart
+++ lib/presentation/pages/text_painter/text_painter_page.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 String get _longText => String.fromCharCodes(
-      List.generate(180, (index) => 12354 + index),
+      List.generate(240, (index) => 12354 + index),
     );
 
-class TextPainterPage extends StatelessWidget {
+class TextPainterPage extends StatefulWidget {
   const TextPainterPage._();
 
   static const routeName = '/text_painter';
@@ -17,35 +17,67 @@ class TextPainterPage extends StatelessWidget {
   }
 
   @override
+  State<TextPainterPage> createState() => _TextPainterPageState();
+}
+
+class _TextPainterPageState extends State<TextPainterPage> {
+  double _horizontalPadding = 0;
+  @override
   Widget build(BuildContext context) {
-    // ! LayoutBuilder 使うのめんどくさいので、padding は設けない
     return Scaffold(
       appBar: AppBar(title: const Text('TextPainter')),
       body: Center(
-        child: Column(
-          children: [
-            Text(
-              _longText,
-              overflow: TextOverflow.ellipsis,
-              maxLines: 3,
-            ),
-            const SizedBox(height: 8),
-            const _EllipsisText(),
-          ],
+        child: Padding(
+          padding: EdgeInsets.all(_horizontalPadding),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <double>[0, 8, 24]
+                    .map(
+                      (padding) => Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 4,
+                        ),
+                        child: FilledButton(
+                          onPressed: () {
+                            setState(() {
+                              _horizontalPadding = padding;
+                            });
+                          },
+                          child: Text('$padding'),
+                        ),
+                      ),
+                    )
+                    .toList(),
+              ),
+              const SizedBox(height: 8),
+              Text(
+                _longText,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 3,
+              ),
+              const SizedBox(height: 8),
+              const _CustomEllipsisText(),
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
-class _EllipsisText extends StatelessWidget {
-  const _EllipsisText();
+class _CustomEllipsisText extends StatelessWidget {
+  const _CustomEllipsisText();
 
   @override
   Widget build(BuildContext context) {
+    final style = DefaultTextStyle.of(context).style;
+
     return LayoutBuilder(
       builder: (context, c) {
         final data = _longText;
+
         final textPainter = TextPainter(
           text: TextSpan(text: data),
           maxLines: 3,
@@ -55,9 +87,15 @@ class _EllipsisText extends StatelessWidget {
         final lastPosition = textPainter.getPositionForOffset(
           Offset(textPainter.width, textPainter.height),
         );
+
+        print('取得した lastPosition の文字列: "${data[lastPosition.offset]}"');
+
         final lastIndex = textPainter.getOffsetBefore(lastPosition.offset);
 
-        return Text(data.substring(0, lastIndex));
+        return Text(
+          data.substring(0, lastIndex),
+          style: style,
+        );
       },
     );
   }
