6b38f64b4fd6441481317f6b7a28d42f98591958
Zudah228 <rezel95.hin@gmail.com>
2024-04-22 09:43:20 +0900

    feat: FractionallyWidgetPage の変更

--- lib/presentation/pages/fractionary_widget/fractionally_widget_page.dart
+++ lib/presentation/pages/fractionary_widget/fractionally_widget_page.dart
@@ -16,65 +16,106 @@ class FractionallyWidgetPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(
-        children: [
-          const Text('FractionallySizedBox'),
-          const SizedBox(
-            height: 100,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                _SizedBox(color: Colors.indigo),
-                FractionallySizedBox(
-                  heightFactor: 0.15,
-                  child: _SizedBox(
-                    color: Colors.green,
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            children: [
+              // FractionallySizedBox
+              const Text('FractionallySizedBox'),
+              const SizedBox(height: 16),
+              const _SizedRowBox(
+                children: [
+                  _SizedBox(color: Colors.indigo),
+                  FractionallySizedBox(
+                    heightFactor: 0.5,
+                    child: _SizedBox(
+                      color: Colors.green,
+                    ),
                   ),
+                  ClipRect(
+                    child: FractionallySizedBox(
+                      heightFactor: 2,
+                      child: _SizedBox(
+                        color: Colors.green,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 100),
+              ColoredBox(
+                color: Colors.grey[300]!,
+                child: const _SizedRowBox(
+                  children: [
+                    _SizedBox(color: Colors.indigo),
+                    Expanded(
+                      child: FractionallySizedBox(
+                        widthFactor: 0.5,
+                        alignment: Alignment.centerLeft,
+                        child: _SizedBox(
+                          color: Colors.green,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+              ColoredBox(
+                color: Colors.grey[300]!,
+                child: const _SizedRowBox(
+                  children: [
+                    _SizedBox(color: Colors.indigo),
+                    Expanded(
+                      child: FractionallySizedBox(
+                        widthFactor: 0.25,
+                        alignment: Alignment.centerLeft,
+                        child: _SizedBox(
+                          color: Colors.green,
+                        ),
+                      ),
+                    ),
+                    _SizedBox(color: Colors.indigo),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 24),
+
+              // Align
+              const Text('Align'),
+              const SizedBox(height: 16),
+              const _SizedRowBox(
+                children: [
+                  _SizedBox(color: Colors.indigo),
+                  ClipRect(
+                    child: Align(
+                      widthFactor: 0.5,
+                      child: _SizedBox(color: Colors.amber),
+                    ),
+                  ),
+                  _SizedBox(color: Colors.indigo),
+                ],
+              ),
+              const SizedBox(height: 24),
+
+              // Transform.scale
+              const Text('Transform.scale'),
+              const SizedBox(height: 16),
+              _SizedRowBox(
+                children: [
+                  const _SizedBox(color: Colors.indigo),
+                  Transform.scale(
+                    scaleX: 0.5,
+                    alignment: Alignment.centerLeft,
+                    child: const _SizedBox(
+                      color: Colors.purple,
+                    ),
+                  ),
+                  const _SizedBox(color: Colors.indigo),
+                ],
+              ),
+            ],
           ),
-          const SizedBox(height: 24),
-          const Text('Align'),
-          const SizedBox(
-            height: 100,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                _SizedBox(color: Colors.indigo),
-                ClipRect(
-                  child: Align(
-                    heightFactor: 0.15,
-                    child: _SizedBox(color: Colors.amber),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(height: 24),
-          const Text('Transform.scale'),
-          SizedBox(
-            height: 100,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const _SizedBox(color: Colors.indigo),
-                Transform.scale(
-                  scaleY: 0.5,
-                  child: const _SizedBox(
-                    color: Colors.purple,
-                  ),
-                ),
-                Transform.scale(
-                  scaleY: 0.5,
-                  child: const _SizedBox(
-                    color: Colors.purple,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
@@ -92,8 +133,25 @@ class _SizedBox extends StatelessWidget {
       child: ColoredBox(
         color: color,
         child: const Center(
-          child: Text('AAA'),
+          child: Text('ABC'),
         ),
+      ),
+    );
+  }
+}
+
+class _SizedRowBox extends StatelessWidget {
+  const _SizedRowBox({required this.children});
+
+  final List<Widget> children;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 100,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: children,
       ),
     );
   }
